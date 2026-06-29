56c20bae93688e7fc5469cbb636519275792358a
marcos-renan <devmarcos7@gmail.com>
2026-06-29 09:19:33 -0300

    feat: ✨ adicionando roteador Gin

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+
+	"github.com/gin-gonic/gin"
+)
+
+func main(){
+
+	router := gin.Default()
+
+	router.GET("/hi", func(c *gin.Context){
+
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
+
+	router.Run() //escuta por padrão na porta 8080
+}
